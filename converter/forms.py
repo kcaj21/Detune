5552e9674757be5912ddeb22b161f9cf61e2f187
kcaj21 <alex.kcaj@gmail.com>
2024-04-12 18:21:06 +0100

    updated view, now to add logic

---
+++ converter/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+
+class YouTubeForm(forms.Form):
+    youtube_url = forms.URLField(label='YouTube Video URL')
--- converter/views.py
+++ converter/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pytube import YouTube
+from .forms import *
 
 def home_view(request):
-    return render(request, 'home.html')
+    form = YouTubeForm()
+    
+    if request.method == 'POST':
+        form = YouTubeForm(request.POST)
+    
+    return render(request, 'home.html', {'form' : form})
+
+#httpstreamresponse
