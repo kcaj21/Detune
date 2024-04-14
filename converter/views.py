28de42e0a1a61f92325e37897ff5448da8e0efca
kcaj21 <alex.kcaj@gmail.com>
2024-04-14 16:50:33 +0100

    can post url and get mp3 donwload

--- converter/views.py
+++ converter/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from pytube import YouTube
 from .forms import *
+import os
 
 def home_view(request):
     form = YouTubeForm()
     
     if request.method == 'POST':
         form = YouTubeForm(request.POST)
+        if form.is_valid():
+            youtube_url = form.cleaned_data["youtube_url"]
+            yt = YouTube(youtube_url)
+            video = yt.streams.filter(only_audio=True).first()
+            video.download(output_path=os.path.join(os.path.expanduser('~'), 'Downloads'))
     
     return render(request, 'home.html', {'form' : form})
 
